17d269f57c82e9c9d6e3034bc90a7d63993910d9
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2024-10-05 12:49:50 +0700

    Solution for given radius value

--- chapter2/exercise2.c
+++ chapter2/exercise2.c
@@ -4,10 +4,7 @@
 int main()
 {
     #define PI_NUMBER 3.14159265358
-    int radius;
-
-    printf("Enter your radius: ");
-    scanf("%d", &radius);
+    int radius = 10;
 
     double volume = 4.0f / 3.0 * PI_NUMBER * pow(radius, 3);
     printf("Volume of a sphere with a 10-meter radius: %lf\n", volume);
