4603609e0cb58f9d0997c88c73382986de6f969e
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2024-10-21 22:08:50 +0700

    fixed

--- chapter4/ex5.c
+++ chapter4/ex5.c
@@ -7,8 +7,7 @@ int main(void) {
 
     // Nhập 11 chữ số từ người dùng
     printf("Enter the first 11 digits of a UPC: ");
-    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d",
-          &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9, &d10, &d11);
+    scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d1, &d2, &d3, &d4, &d5, &d6, &d7, &d8, &d9, &d10, &d11);
 
     // Tính tổng các chữ số theo quy tắc UPC
     first_sum = d1 + d3 + d5 + d7 + d9 + d11; // Tổng các chữ số lẻ
