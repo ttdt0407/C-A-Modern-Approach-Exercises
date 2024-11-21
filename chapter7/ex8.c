ed1fbcf245c63f3355edf8d9123b818ff5930039
Ta Tran Dinh Tien <tatrandinhtien@gmail.com>
2024-11-21 11:41:26 +0700

    ex8

---
+++ chapter7/ex8.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <ctype.h>
+
+
+int main()
+{
+    int hour, minute;
+    int convert;
+    char am_pm1, am_pm2;
+    printf("Enter a 12-hour time (x:y): \n");
+    scanf("%d:%d %c%c", &hour, &minute, &am_pm1, &am_pm2);
+
+    am_pm1 = toupper(am_pm1);
+    am_pm2 = toupper(am_pm2);
+    
+    if (am_pm1 == 'P' && hour < 12)
+    hour+= 12;
+
+    else if (am_pm1 == 'A' && hour == 12)
+    hour = 0;
+
+    convert = hour * 60 + minute;
+
+    if (convert <= 532)
+    {
+        printf("Closest departure time is 8:00 a.m., arriving at 10:16 a.m.");
+    }
+    else if(convert <= 631)
+    {
+        printf("Closest departure time is 9:43 a.m., arriving at 11:52 a.m.");        
+    }
+     else if(convert <= 723)
+    {
+        printf("Closest departure time is 11:19 a.m., arriving at 1:31 p.m.");
+        
+    }
+     else if(convert <= 804)
+    {
+        printf("Closest departure time is 12:47 p.m., arriving at 3:00 p.m.");
+        
+    }
+     else if(convert <= 893)
+    {
+        printf("Closest departure time is 2:00 p.m., arriving at 4:08 p.m.");
+    }
+     else if(convert <= 1043)
+    {
+        printf("Closest departure time is 3:45 p.m., arriving at 5:55 p.m.");
+    }
+     else if(convert <= 1223)
+    {
+        printf("Closest departure time is 7:00 p.m., arriving at 9:20 p.m.");
+    }
+    else
+    {
+        printf("Closest departure time is 9:45 p.m., arriving at 11:58 p.m.");
+    }
+    return 0;
+}
