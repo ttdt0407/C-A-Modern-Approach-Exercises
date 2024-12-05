c2a2d1a38c091ba07f93719f1b3d96d28ed3b968
Tien <tatrandinhtien@gmail.com>
2024-12-05 14:46:08 +0700

    medium exercise

---
+++ chapter8/ex14.c
@@ -0,0 +1,52 @@
+// Write a program that reverses the words in a sentence:
+//  Enter a sentence: you can cage a swallow can't you?
+//  Reversal of sentence: you can't swallow a cage can you?
+#include <stdio.h>
+
+#include <ctype.h>
+int main(void)
+{
+    char ch;
+    int index = 0;
+    int jump;
+    int indexMark;
+    char sentence[50];
+    char punctuation = '\0';
+
+    printf("Enter a sentence: ");
+
+    while((ch=getchar()) != '\n')
+    {   
+        if (ch == '.' || ch == '!' || ch == '?')
+        {
+            punctuation = ch;
+            break;
+        }
+        sentence[index++] = ch;
+        
+    }
+    sentence[index] = '\0';
+    indexMark = index;
+    index--;
+
+    printf("Reversal of sentence: ");
+    while(index >= 0)
+    {   
+        
+        if(sentence[index] == ' ' || index == 0)
+        {
+            jump = (index == 0) ? index : index + 1;
+
+            while(sentence[jump] != ' ' && sentence[jump] != '\0')
+                printf("%c", sentence[jump++]);
+            if(index > 0)
+            printf(" ");
+        }
+        index--;
+        
+    }
+    if (punctuation != '\0')
+    printf("%c", punctuation);
+
+    return 0;
+}
